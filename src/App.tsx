af827c835ddc3cb6ad387c69467d8b457e4bb59a
inada <inada.s@marietta.co.jp>
2021-12-03 00:16:40 +0900

    add

--- src/App.tsx
+++ src/App.tsx
@@ -7,68 +7,81 @@ import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import "./css/style.css";
 
+type TaskList = {
+    info: string,
+    checked: boolean
+}
+
 export const App = () => {
 
-  const [inputTextState, setInputTextState] = useState<string>("");
+    const [inputTextState, setInputTextState] = useState<string>("");
 
-  const [taskList, setTaskList] = useState<string[]>([]);
+    const [taskList, setTaskList] = useState<string[]>([]);
 
-  const handlePressEnter = useCallback((e) => {
-    if (e.key === "Enter") {
-      setTaskList([...taskList, inputTextState]);
-      setInputTextState("");
+    const [taskCheckedList, setTaskCheckedList] = useState<boolean[]>([]);
 
-    }
-  }, [inputTextState, taskList]);
+    const handlePressEnter = useCallback((e) => {
+        if (e.key === "Enter" && inputTextState !== "") {
+            setTaskList([...taskList, inputTextState]);
+            setTaskCheckedList([...taskCheckedList, false]);
+            setInputTextState("");
+        }
+    }, [inputTextState, taskList]);
 
-  const handleChangeInputElement = useCallback((e) => {
-    setInputTextState(e.target.value);
-  }, []);
+    const handleChangeInputElement = useCallback((e) => {
+        setInputTextState(e.target.value);
+    }, []);
 
-  return (
-    <>
-      <Box sx={{flexGrow: 1}}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-              TASKER
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <div style={{marginBottom: 20}}/>
-      <Grid container spacing={3}>
-        <Grid item xs>
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            fullWidth
-            id="standard-basic"
-            label="タスクを入力"
-            variant="standard"
-            value={inputTextState}
-            onChange={(e) => handleChangeInputElement(e)}
-            onKeyPress={(e) => handlePressEnter(e)}
-          />
-          {
-            taskList.length === 0
-              ? <></>
-              : taskList.map((info,i) => {
-                return (
-                  <>
-                    <div style={{marginTop: 10}}>
-                      <input type="checkbox" id={`cbtest${i}`}/>
-                      <label htmlFor={`cbtest${i}`} className="check-box"/>
-                      <span>{info}</span>
-                    </div>
-                  </>
-                )
-              })
-          }
-        </Grid>
-        <Grid item xs>
-        </Grid>
-      </Grid>
-    </>
-  )
+    return (
+        <>
+            <Box sx={{flexGrow: 1}}>
+                <AppBar position="static" color="default">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                            TASKER
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <div style={{marginBottom: 20}}/>
+            <Grid container spacing={3}>
+                <Grid item xs>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        fullWidth
+                        id="standard-basic"
+                        label="please input task"
+                        variant="standard"
+                        size="medium"
+                        value={inputTextState}
+                        onChange={(e) => handleChangeInputElement(e)}
+                        onKeyPress={(e) => handlePressEnter(e)}
+                    />
+                    {
+                        taskList.length === 0
+                            ? <></>
+                            : taskList.map((info, i) => {
+                                return (
+                                    <>
+                                        <input
+                                            id={`four${i}`}
+                                            type='checkbox'
+                                            checked={taskCheckedList[i]}
+                                        />
+                                        <label htmlFor={`four${i}`}>
+                                            <span></span>
+                                            {info}
+                                            <ins><i>{info}</i></ins>
+                                        </label>
+                                    </>
+                                )
+                            })
+                    }
+                </Grid>
+                <Grid item xs>
+                </Grid>
+            </Grid>
+        </>
+    )
 }
