4f166b8f2b4747f818654f03f05aeaf6feb253a1
稲田@marietta <inada.s@marietta.co.jp>
2021-12-02 18:20:48 +0900

    [Add]

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import React, {useCallback} from 'react';
+import React, {useState, useCallback} from 'react';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
+import "./css/style.css";
 
 export const App = () => {
 
+  const [inputTextState, setInputTextState] = useState<string>("");
+
+  const [taskList, setTaskList] = useState<string[]>([]);
+
   const handlePressEnter = useCallback((e) => {
     if (e.key === "Enter") {
-      console.log('dkdk');
+      setTaskList([...taskList, inputTextState]);
+      setInputTextState("");
+
     }
+  }, [inputTextState, taskList]);
+
+  const handleChangeInputElement = useCallback((e) => {
+    setInputTextState(e.target.value);
   }, []);
 
   return (
@@ -30,7 +41,30 @@ export const App = () => {
         <Grid item xs>
         </Grid>
         <Grid item xs={6}>
-          <TextField fullWidth id="standard-basic" label="タスクを入力" variant="standard" onKeyPress={(e) => handlePressEnter(e)}/>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="タスクを入力"
+            variant="standard"
+            value={inputTextState}
+            onChange={(e) => handleChangeInputElement(e)}
+            onKeyPress={(e) => handlePressEnter(e)}
+          />
+          {
+            taskList.length === 0
+              ? <></>
+              : taskList.map((info,i) => {
+                return (
+                  <>
+                    <div style={{marginTop: 10}}>
+                      <input type="checkbox" id={`cbtest${i}`}/>
+                      <label htmlFor={`cbtest${i}`} className="check-box"/>
+                      <span>{info}</span>
+                    </div>
+                  </>
+                )
+              })
+          }
         </Grid>
         <Grid item xs>
         </Grid>
