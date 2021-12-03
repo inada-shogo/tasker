595db5b61b1946ba99ab57e404c4c4147c81e86a
稲田@marietta <inada.s@marietta.co.jp>
2021-12-03 16:24:44 +0900

    [add]

--- src/App.tsx
+++ src/App.tsx
@@ -6,103 +6,99 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import "./css/style.css";
-import { info } from 'console';
 
 type TaskList = {
-    info: string,
-    checked: boolean
+  text: string,
+  checked: boolean
 }
 
 export const App = () => {
 
-    const [inputTextState, setInputTextState] = useState<string>("");
+  const [inputTextState, setInputTextState] = useState<string>("");
 
-    const [taskList, setTaskList] = useState<string[]>([]);
-    const [taskCheckedList, setTaskCheckedList] = useState<boolean[]>([]);
+  const [taskList, setTaskList] = useState<TaskList[]>([]);
 
-    const handlePressEnter = useCallback((e) => {
-        if (e.key === "Enter" && inputTextState !== "") {
-            setTaskList([...taskList, inputTextState]);
-            setInputTextState("");
-        }
-    }, [inputTextState, taskList]);
+  const handlePressEnter = useCallback((e) => {
+    if (e.key === "Enter" && inputTextState !== "") {
+      setTaskList([...taskList, {text: inputTextState, checked: false}]);
+      setInputTextState("");
+    }
+  }, [inputTextState, taskList]);
 
-    const handleChangeInputElement = useCallback((e) => {
-        setInputTextState(e.target.value);
-    }, []);
-
-    const handleChangeCheckedFlg = useCallback((clickNum: number) => {
-        setTaskCheckedList(
-            taskCheckedList.map((info, i) => {
-                return i === clickNum ? !taskCheckedList[i] : info;
-            })
-        )
-    }, [taskCheckedList]);
-
-    const handleDeleteInputFunction = useCallback((index: number) => {
-        console.log(index);
-    }, []);
-
-    const handleInputClickEvent = useCallback((event: React.MouseEvent<HTMLInputElement>, activeEventNum: number) => {
-        if (event.ctrlKey) {
-            handleDeleteInputFunction(activeEventNum);
-        }
-    }, [handleDeleteInputFunction]);
-
-    return (
-        <>
-            <Box sx={{flexGrow: 1}}>
-                <AppBar position="static" color="default">
-                    <Toolbar>
-                        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                            TASKER
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-            </Box>
-            <div style={{marginBottom: 20}}/>
-            <Grid container spacing={3}>
-                <Grid item xs>
-                </Grid>
-                <Grid item xs={6}>
-                    <TextField
-                        fullWidth
-                        id="standard-basic"
-                        label="please input task"
-                        variant="standard"
-                        size="medium"
-                        value={inputTextState}
-                        onChange={(e) => handleChangeInputElement(e)}
-                        onKeyPress={(e) => handlePressEnter(e)}
-                    />
-                    {
-                        taskList.length === 0
-                            ? <></>
-                            : taskList.map((info, i) => {
-                                return (
-                                    <>
-                                        <input
-                                            id={`four${i}`}
-                                            type='checkbox'
-                                            checked={taskCheckedList[i]}
-                                            onChange={() => {
-                                                handleChangeCheckedFlg(i)
-                                            }}
-                                            onClick={(e) => handleInputClickEvent(e, i)}
-                                        />
-                                        <label htmlFor={`four${i}`}>
-                                            <span></span>
-                                            {info}
-                                            <ins><i>{info}</i></ins>
-                                        </label>
-                                    </>
-                                )
-                            })
-                    }
-                </Grid>
-                <Grid item xs>
-                </Grid>
-            </Grid>
-        </>
+  const handleChangeCheckedFlg = useCallback((clickNum: number) => {
+    setTaskList(
+      taskList.map((info, i) => {
+        return i === clickNum
+          ? {text: info.text, checked: !info.checked}
+          : info;
+      })
     )
+  }, [taskList]);
+
+  const handleDeleteInputFunction = useCallback((clickNum: number) => {
+    setTaskList(taskList.splice(clickNum, 1));
+  }, [taskList]);
+
+  const handleInputClickEvent = useCallback((event: React.MouseEvent<HTMLInputElement>, activeEventNum: number) => {
+    if (event.ctrlKey) {
+      handleDeleteInputFunction(activeEventNum);
+    }
+  }, [handleDeleteInputFunction]);
+
+  return (
+    <>
+      <Box sx={{flexGrow: 1}}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+              TASKER
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div style={{marginBottom: 20}}/>
+      <Grid container spacing={2}>
+        <Grid item xs>
+        </Grid>
+        <Grid item xs={8}>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="please input task"
+            variant="standard"
+            size="medium"
+            autoComplete='off'
+            value={inputTextState}
+            color={"success"}
+            onChange={(e) => setInputTextState(e.target.value)}
+            onKeyPress={(e) => handlePressEnter(e)}
+          />
+          {
+            taskList.length === 0
+              ? <></>
+              : taskList.map((info, i) => {
+                return (
+                  <>
+                    <input
+                      id={`four${i}`}
+                      type='checkbox'
+                      checked={info.checked}
+                      onChange={() => handleChangeCheckedFlg(i)}
+                      onClick={(e) => handleInputClickEvent(e, i)}
+                    />
+                    <label htmlFor={`four${i}`}>
+                      <span></span>
+                      {info.text}
+                      <ins><i>{info.text}</i></ins>
+                    </label>
+                  </>
+                )
+              })
+          }
+        </Grid>
+        <Grid item xs>
+        </Grid>
+      </Grid>
+    </>
+  )
 }
